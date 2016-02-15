1c34d8ec30df1953e73313e2b4c616c0735384ae
Miroslav Vitanov <mvvitanov@gmail.com>
2016-02-15 10:15:48 +0200

    Reflect the changes of the class

--- tests/TextareaFieldTest.php
+++ tests/TextareaFieldTest.php
@@ -9,8 +9,12 @@ class TextareaFieldTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function it_renders_the_full_html()
     {
-        $field = new Textarea('test', 'Test');
+        $field = new Textarea('test', 'Test', ['value' => '']);
 
         $this->assertSame('<label for="test">Test</label>'."\n".'<textarea id="test" name="test"></textarea>', $field->render());
     }
+}
+
+function old($field) {
+    return;
 }
