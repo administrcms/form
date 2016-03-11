2d851a801ea7963f5141ec010af7c397b7aef9b0
Miroslav Vitanov <mvvitanov@gmail.com>
2016-03-11 09:59:31 +0200

    Fix file test

--- tests/FileFieldTest.php
+++ tests/FileFieldTest.php
@@ -9,6 +9,6 @@ class FileFieldTest extends PHPUnit_Framework_TestCase
     {
         $field = new File('test', 'Test');
 
-        $this->assertSame('<label for="test">Test</label>'."\n".'<input type="file" id="test" name="test">', $field->render());
+        $this->assertSame('<label for="test">Test</label>'."\n".'<input type="file" id="test" name="test" value="">', $field->render());
     }
 }
