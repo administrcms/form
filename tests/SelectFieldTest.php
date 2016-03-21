b3d22bdce51774a1070a1ad56e07487242e875cc
Miroslav Vitanov <mvvitanov@gmail.com>
2016-03-21 12:25:37 +0200

    Fix test to reflect value name change

--- tests/SelectFieldTest.php
+++ tests/SelectFieldTest.php
@@ -16,7 +16,7 @@ class SelectFieldTest extends PHPUnit_Framework_TestCase
     public function it_renders_with_options_the_full_html()
     {
         $field = new Select('test', 'Test', [
-            'value' => ['miro' => 'test'],
+            'values' => ['miro' => 'test'],
         ]);
 
         $this->assertSame('<label for="test">Test</label>'."\n".'<select id="test" name="test"><option value="miro">test</option></select>', $field->render());
