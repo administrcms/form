99f0f7ba88e266f443f92e4da5e358141435ae6e
Miroslav Vitanov <mvvitanov@gmail.com>
2016-06-08 17:48:34 +0300

    Add test for selected state of select

--- tests/SelectFieldTest.php
+++ tests/SelectFieldTest.php
@@ -21,4 +21,18 @@ class SelectFieldTest extends PHPUnit_Framework_TestCase
 
         $this->assertSame('<label for="test">Test</label>'."\n".'<select id="test" name="test"><option value="miro">test</option></select>', $field->render());
     }
+    
+    /** @test */
+    public function it_marks_the_checked_option()
+    {
+        $field = new Select('test', 'Test', [
+            'values' => [
+                'miro' => 'test',
+                'test' => 'miro'
+            ],
+            'value' => 'miro'
+        ]);
+
+        $this->assertSame('<label for="test">Test</label>'."\n".'<select id="test" name="test"><option value="miro" selected="selected">test</option><option value="test">miro</option></select>', $field->render());
+    }
 }
