4e2e3d8da2c9a1708f73c136758787ff00e60487
Miroslav Vitanov <mvvitanov@gmail.com>
2016-06-08 17:26:22 +0300

    Fix radio button test coverage

--- tests/RadioFieldTest.php
+++ tests/RadioFieldTest.php
@@ -11,4 +11,13 @@ class RadioFieldTest extends PHPUnit_Framework_TestCase
 
         $this->assertSame('<label for="test">Test</label>'."\n".'<input type="radio" id="test" name="test" value="">', $field->render());
     }
+
+    /** @test */
+    public function it_sets_checked_state()
+    {
+        $field = new Radio('test', 'Test', ['value' => 'should_be_checked']);
+        $field->setValue('should_be_checked');
+
+        $this->assertSame('<input type="radio" id="test" name="test" value="should_be_checked" checked="checked">', $field->renderField());
+    }
 }
