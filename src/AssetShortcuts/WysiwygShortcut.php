05ae186bf934b3b14e5cf724fa59bda1e760c194
Miroslav Vitanov <mvvitanov@gmail.com>
2016-07-01 16:03:37 +0300

    Add priorities

--- src/AssetShortcuts/WysiwygShortcut.php
+++ src/AssetShortcuts/WysiwygShortcut.php
@@ -10,11 +10,13 @@ class WysiwygShortcut implements Shortcut
     public function execute()
     {
         Asset::addJs(
-            '/vendor/administr/form/tinymce/tinymce.min.js'
+            '/vendor/administr/form/tinymce/tinymce.min.js',
+            10
         );
 
         Asset::addJs(
-            '/vendor/administr/form/form.js'
+            '/vendor/administr/form/form.js',
+            0
         );
     }
 }
