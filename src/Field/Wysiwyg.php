9f71a6140056ef23d364df72435fa01a6dc49d43
Miroslav Vitanov <mvvitanov@gmail.com>
2016-07-01 16:03:56 +0300

    Fix the addition of a class

--- src/Field/Wysiwyg.php
+++ src/Field/Wysiwyg.php
@@ -9,9 +9,12 @@ class Wysiwyg extends Textarea
     public function __construct($name, $label, array $options)
     {
         Asset::wysiwyg();
-
-        $options['class'] = array_get($options, 'class') . ' administr-wysiwyg';
-
         parent::__construct($name, $label, $options);
+    }
+
+    public function renderField(array $attributes = [])
+    {
+        $attributes['class'] = array_get($attributes, 'class') . ' administr-wysiwyg';
+        return parent::renderField($attributes);
     }
 }
