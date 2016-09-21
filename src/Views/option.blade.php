a6d092e2a5e18951fd0e9af9493dc4fd1ca66c0e
Miroslav Vitanov <mvvitanov@gmail.com>
2016-09-21 11:06:38 +0300

    Fix definition of option

--- src/Views/option.blade.php
+++ src/Views/option.blade.php
@@ -1,1 +1,1 @@
-<option id="{{ $field->getName() }}" name="{{ $field->getName() }}" {!! $field->attributes() !!}>{{ $field->getValue() }}</option>
+<option id="{{ $field->getName() }}" name="{{ $field->getName() }}" {!! $field->attributes() !!}>{{ $field->getLabel() }}</option>
