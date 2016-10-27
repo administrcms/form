0c97062672703ba8d29293fa0d2d6a8c481fa37c
Kaukov <Kaukov@users.noreply.github.com>
2016-10-27 12:44:46 +0300

    Update Select field to keep its old values

--- src/Field/Select.php
+++ src/Field/Select.php
@@ -6,32 +6,45 @@ class Select extends AbstractType
 {
     protected $selectOptions = [];
 
-    public function render(array $attributes = [])
+    public function render(array $attributes = [], array $viewData = [])
     {
         $this->options = array_merge($this->options, $attributes);
 
-        $value = $this->getOption('value');
+        $value = old(str_replace('[]', '', $this->name), $this->getOption('value'));
         unset($this->options['value']);
 
-        if ($values = $this->getOption('values')) {
-            $this->selectOptions = [];
+        $this->setSelectOptions($value);
 
-            foreach ($values as $optionValue => $display) {
-                $optionAttrs = [];
+        return parent::render($attributes, $viewData);
+    }
 
-                if ($optionValue === $value) {
-                    $optionAttrs['selected'] = 'selected';
-                }
-
-                $this->selectOptions[] = new Option($optionValue, $display, $optionAttrs);
-            }
-        }
-
-        return parent::render();
+    public function option($value, $display, $options = null)
+    {
+        $this->selectOptions[] = new Option($value, $display, $options);
+        return $this;
     }
 
     public function options()
     {
         return $this->selectOptions;
+    }
+
+    protected function setSelectOptions($value)
+    {
+        if (!$values = $this->getOption('values')) {
+            return;
+        }
+
+        $this->selectOptions = [];
+
+        foreach ($values as $optionValue => $display) {
+            $optionAttrs = [];
+
+            if ($optionValue == $value || is_array($value) && in_array($optionValue, $value)) {
+                $optionAttrs['selected'] = 'selected';
+            }
+
+            $this->option($optionValue, $display, $optionAttrs);
+        }
     }
 }
