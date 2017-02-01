b38339468f838d6c023f3394ef9d5912b8e3e760
Miroslav Vitanov <mvvitanov@gmail.com>
2017-02-01 16:26:28 +0200

    Fix order of attributes

--- src/Views/checkbox.blade.php
+++ src/Views/checkbox.blade.php
@@ -1,3 +1,3 @@
 <label for="{{ $field->getName() }}">{{ $field->getLabel() }}</label>
-<input type="checkbox" id="{{ $field->getName() }}" name="{{ $field->getName() }}" value="{{ $field->getValue() }}" {!! $field->attributes() !!}>
+<input type="checkbox" {!! $field->attributes() !!} id="{{ $field->getName() }}" name="{{ $field->getName() }}" value="{{ $field->getValue() }}">
 <span>{{ $errors->first($field->getName()) }}</span>
--- src/Views/radio.blade.php
+++ src/Views/radio.blade.php
@@ -1,3 +1,3 @@
 <label for="{{ $field->getName() }}">{{ $field->getLabel() }}</label>
-<input type="radio" id="{{ $field->getName() }}" name="{{ $field->getName() }}" value="{{ $field->getValue() }}" {!! $field->attributes() !!}>
+<input type="radio" {!! $field->attributes() !!} id="{{ $field->getName() }}" name="{{ $field->getName() }}" value="{{ $field->getValue() }}">
 <span>{{ $errors->first($field->getName()) }}</span>
